55aea77fcd51f7f006bc3f80ed2fa93ddbe3762f
Zikang Zhao <z73zhao@uwaterloo.ca>
2017-03-24 04:41:48 -0400

    add rest endpoint to monitor the changes

--- app/app.js
+++ app/app.js
@@ -7,12 +7,13 @@ var _ftpWatcher2 = _interopRequireDefault(_ftpWatcher);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var log = require("winston");
-var argv = require('optimist').argv;
+var argv = require("optimist").argv;
+var Hapi = require("hapi");
 
-function run() {
+function runByCmd() {
     var ftpWatcher = new _ftpWatcher2.default();
     var config = {
-        host: argv.host,
+        host: argv.host.replace("ftp://", "").slice(0, -1),
         port: argv.port,
         user: argv.user,
         password: argv.password
@@ -33,4 +34,72 @@ function run() {
     });
 }
 
-run();
+function runByEndpoint() {
+    var ftpWatcher = new _ftpWatcher2.default();
+    var config = {
+        host: argv.host.replace("ftp://", "").slice(0, -1),
+        port: argv.port,
+        user: argv.user,
+        password: argv.password
+    };
+
+    var interval = argv.interval || 10;
+    var path = argv.path || "/";
+    var sync = argv.mode === "watch" ? ftpWatcher.watch(path, interval) :
+    // ftpWatcher.listen(path, interval, _ => log.info(`Inserted: ${_.name}`), _ => log.info(`Deleted: ${_.name}`))
+    ftpWatcher.listenBatch(path, interval, function (_) {
+        return log.info("Inserted: \n" + JSON.stringify(_, null, 2));
+    }, function (_) {
+        return log.info("Deleted: \n" + JSON.stringify(_, null, 2));
+    });
+
+    Promise.all([ftpWatcher.init(config), sync]).catch(function (error) {
+        return log.error(error);
+    });
+}
+
+function launchServer() {
+    var server = new Hapi.Server();
+    server.connection({ port: 80, host: "localhost" });
+
+    server.route({
+        method: "POST",
+        path: "/{mode}",
+        handler: function handler(request, reply) {
+            var ftpWatcher = new _ftpWatcher2.default();
+            var payload = request.payload;
+            if (!payload.host) {
+                reply(JSON.stringify({ res: "missing host" })).code(404);
+                return;
+            }
+            var config = {
+                host: payload.host.replace("ftp://", "").slice(0, -1),
+                port: payload.port,
+                user: payload.user,
+                password: payload.password
+            };
+            var interval = payload.interval || 10;
+            var path = payload.path || "/";
+
+            var sync = payload.mode === "watch" ? ftpWatcher.watch(path, interval) : ftpWatcher.listenBatch(path, interval, function (_) {
+                return log.info("Inserted: \n" + JSON.stringify(_, null, 2));
+            }, function (_) {
+                return log.info("Deleted: \n" + JSON.stringify(_, null, 2));
+            });
+
+            Promise.all([ftpWatcher.init(config), sync]).catch(function (error) {
+                return log.error(error);
+            });
+            reply(JSON.stringify({ res: "success" }));
+        }
+    });
+
+    server.start(function (error) {
+        if (error) {
+            throw error;
+        }
+        log.info("Server running at: " + server.info.uri);
+    });
+}
+
+launchServer();
--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import FtpWatcher from "./ftpWatcher"
 
 const log = require("winston")
-const argv = require('optimist').argv
+const argv = require("optimist").argv
+const Hapi = require("hapi")
 
-function run() {
+function runByCmd() {
     const ftpWatcher = new FtpWatcher()
     const config = {
-        host: argv.host,
+        host: argv.host.replace("ftp://", "").slice(0, -1),
         port: argv.port,
         user: argv.user,
         password: argv.password,
@@ -25,4 +26,69 @@ function run() {
     ]).catch(error => log.error(error))
 }
 
-run()
+function runByEndpoint() {
+    const ftpWatcher = new FtpWatcher()
+    const config = {
+        host: argv.host.replace("ftp://", "").slice(0, -1),
+        port: argv.port,
+        user: argv.user,
+        password: argv.password,
+    }
+
+    const interval = argv.interval || 10
+    const path = argv.path || "/"
+    const sync = argv.mode === "watch" ?
+        ftpWatcher.watch(path, interval) :
+        // ftpWatcher.listen(path, interval, _ => log.info(`Inserted: ${_.name}`), _ => log.info(`Deleted: ${_.name}`))
+        ftpWatcher.listenBatch(path, interval, _ => log.info(`Inserted: \n${JSON.stringify(_, null, 2)}`), _ => log.info(`Deleted: \n${JSON.stringify(_, null, 2)}`))
+
+    Promise.all([
+        ftpWatcher.init(config),
+        sync,
+    ]).catch(error => log.error(error))
+}
+
+function launchServer() {
+    const server = new Hapi.Server()
+    server.connection({ port: 80, host: "localhost" })
+
+    server.route({
+        method: "POST",
+        path: "/{mode}",
+        handler: (request, reply) => {
+            const ftpWatcher = new FtpWatcher()
+            const payload = request.payload
+            if (!payload.host) {
+                reply(JSON.stringify({ res: "missing host" })).code(404)
+                return
+            }
+            const config = {
+                host: payload.host.replace("ftp://", "").slice(0, -1),
+                port: payload.port,
+                user: payload.user,
+                password: payload.password,
+            }
+            const interval = payload.interval || 10
+            const path = payload.path || "/"
+
+            const sync = payload.mode === "watch" ?
+                ftpWatcher.watch(path, interval) :
+                ftpWatcher.listenBatch(path, interval, _ => log.info(`Inserted: \n${JSON.stringify(_, null, 2)}`), _ => log.info(`Deleted: \n${JSON.stringify(_, null, 2)}`))
+
+            Promise.all([
+                ftpWatcher.init(config),
+                sync,
+            ]).catch(error => log.error(error))
+            reply(JSON.stringify({ res: "success" }))
+        },
+    })
+
+    server.start((error) => {
+        if (error) {
+            throw error
+        }
+        log.info(`Server running at: ${server.info.uri}`)
+    })
+}
+
+launchServer()
